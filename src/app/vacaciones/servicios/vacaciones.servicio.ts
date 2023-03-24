b8946f88f5d94b124041e5b81e64fa92899c8275
MARIO APARICIO MAGDALENA <mario.aparicio.ext@inetum.com>
2023-03-24 12:15:44 +0100

    otra vez

--- src/app/vacaciones/servicios/vacaciones.servicio.ts
+++ src/app/vacaciones/servicios/vacaciones.servicio.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { Vacaciones } from "./modelos/vacaciones";
+import { Vacaciones } from "../../modelos/vacaciones";
 
 
 @Injectable({
