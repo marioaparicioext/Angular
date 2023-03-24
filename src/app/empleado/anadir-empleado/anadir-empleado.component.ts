9f1bdf1421f6ff064e734f69d3e90b702cf95170
MARIO APARICIO MAGDALENA <mario.aparicio.ext@inetum.com>
2023-03-24 13:27:39 +0100

    actualización clase anadir empleado

--- src/app/empleado/anadir-empleado/anadir-empleado.component.ts
+++ src/app/empleado/anadir-empleado/anadir-empleado.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/modelos/empleado';
+import { EmpleadoServicio } from '../servicios/empleado.servicio';
 
 @Component({
   selector: 'app-anadir-empleado',
   templateUrl: './anadir-empleado.component.html',
   styleUrls: ['./anadir-empleado.component.css']
 })
-export class AnadirEmpleadoComponent {
-
+export class AnadirEmpleadoComponent implements OnInit {
+  
+  constructor(private empleadoServicio: EmpleadoServicio){
+  }
+  ngOnInit(): void {
+    
+  }
+    public anadirEmpleado(empleado: Empleado): void {
+    this.empleadoServicio.anadirEmpleado(empleado).subscribe(
+      () => {
+        //duda
+        this.anadirEmpleado(empleado);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+  
+   
 }
