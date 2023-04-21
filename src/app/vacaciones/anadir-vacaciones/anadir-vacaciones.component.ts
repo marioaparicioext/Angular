bb80a951de99ddb300f54227b1a246822db34557
Rafael-INE <rafael.alonso.ext@inetum.com>
2023-04-21 12:52:02 +0200

    Fcehas ya elegidas deshabilitadas, nuevo conteo de fechas en fin de semana

--- src/app/vacaciones/anadir-vacaciones/anadir-vacaciones.component.ts
+++ src/app/vacaciones/anadir-vacaciones/anadir-vacaciones.component.ts
@@ -15,6 +15,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AnadirVacacionesComponent {
   vacaciones = new Vacaciones();
+  vacacionesEmpleado: Vacaciones[];
+  fechasInhabilitadas: Date[];
   hoy = new Date();
   limite: Date;
   empleadoId: number
@@ -22,20 +24,76 @@ export class AnadirVacacionesComponent {
     private empleadoServicio: EmpleadoServicio,
     private router: Router,
     private dateAdapter: DateAdapter<Date>) {
-    this.empleadoId = +localStorage.getItem("id")!;
-  }
 
+    this.empleadoId = +localStorage.getItem("id")!;
+    
+    this.vacacionesServicio.obtenerVacacionesPorEmpleado(this.empleadoId).subscribe(
+      (response: Vacaciones[]) => {
+        this.vacacionesEmpleado = response;
+        console.log(this.vacacionesEmpleado);
+        this.fechasInhabilitadas = this.deshabilitarRangos(this.vacacionesEmpleado);
+        this.fechasInhabilitadas.sort((a,b)=> a.getTime() - b.getTime())
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+
+  }
+  deshabilitarRangos(vacacionesEmpleado: Vacaciones[]): Date[] {
+    this.dateAdapter.setLocale('es');
+    let dateArray: Date[] = [];
+    vacacionesEmpleado.forEach(solicitud => {
+      const fechaInicial = new Date(solicitud.fechaInicio);
+      const fechaFin = new Date(solicitud.fechaFin);
+      while (fechaInicial <= fechaFin) {
+        dateArray.push(new Date(fechaInicial));
+        // Use UTC date to prevent problems with time zones and DST
+        fechaInicial.setDate(fechaInicial.getDate() + 1);
+      }
+    });
+    console.log(dateArray);
+    return dateArray;
+  }
+  
 
   weekendsDatesFilter = (d: Date): boolean => {
     const day = d.getDay();
+    const time = d.getTime();
     /* Prevent Saturday and Sunday for select. */
-    return day !== 0 && day !== 6;
+    return day !== 0 && day !== 6 && !this.fechasInhabilitadas.find(x=>x.getTime()==time);
+  }
+
+  public obtenerNumeroDiasFinDeSemana(date: Date, numeroDiasVacaciones: number){
+    var weekendDayCount = 0;
+    const dateCopy = new Date(date);
+    const toDate = new Date(date);
+    console.log(toDate);
+    toDate.setDate(toDate.getDate()+numeroDiasVacaciones);
+    while(dateCopy < toDate){
+        dateCopy.setDate(dateCopy.getDate() + 1);
+        if(dateCopy.getDay() === 0 || dateCopy.getDay() == 6){
+            ++weekendDayCount;
+        }
+    }
+
+    return weekendDayCount ;
   }
 
   marcarLimite(date: Date){
     const tomorrow = new Date(date);
-    const numeroDeshabilitados =2;
-    tomorrow.setDate(date.getDate()+6+numeroDeshabilitados);
+    let numeroDeshabilitados: number;
+    const numeroDiasVacaciones=7;
+    numeroDeshabilitados = this.obtenerNumeroDiasFinDeSemana(date, numeroDiasVacaciones);
+    console.log("HAY DIAS DE FINDE",numeroDeshabilitados);
+    // if(this.fechasInhabilitadas.length!=0){
+    //   this.fechasInhabilitadas.forEach(dia => {
+    //   });
+      
+    // }
+    console.log("FECHA FIN",tomorrow);
+    tomorrow.setDate(date.getDate()+numeroDiasVacaciones-1+numeroDeshabilitados);
+    console.log("FECHA FIN",tomorrow);
     this.limite = tomorrow;
     let currentDate = new Date(date);
     let dateSent = new Date(this.limite);
@@ -69,3 +127,4 @@ export class AnadirVacacionesComponent {
     });
   }
 }
+
